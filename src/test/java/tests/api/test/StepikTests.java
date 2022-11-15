4bc8b6868596c82a68e74615f257253e19334c72
Dmitriy Lyubtsov <dlyubtsov@rvision.ru>
2022-11-16 02:44:05 +0500

    work in progress

--- src/test/java/tests/api/test/StepikTests.java
+++ src/test/java/tests/api/test/StepikTests.java
@@ -25,7 +25,7 @@ public class StepikTests extends ApiTestBase {
     }
 
     @Test
-    @DisplayName("Регистрация")
+    @DisplayName("Регистрация по API")
     public void registration() {
         stepikPage.registration();
     }
